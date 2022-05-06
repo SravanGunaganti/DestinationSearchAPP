e5ec53370c9c6118f485327f44002b1dba070c49
SravanGunaganti <gunagantisravan376@gmail.com>
2022-05-06 08:05:47 +0000

    first commit

---
+++ src/components/DestinationItem/index.js
@@ -0,0 +1,19 @@
+// Write your code here
+import {Component} from 'react'
+
+import './index.css'
+
+class DestinationItem extends Component {
+  render() {
+    const {destinationItem} = this.props
+    const {imgUrl, name} = destinationItem
+    return (
+      <li className="searchItem">
+        <img src={imgUrl} alt={name} className="image" />
+        <p className="name">{name}</p>
+      </li>
+    )
+  }
+}
+
+export default DestinationItem
